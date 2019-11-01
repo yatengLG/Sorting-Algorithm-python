8341d2f7f787c86a8221f538eb4e31a6a9dfd12b
yatengLG <767624851@qq.com>
2019-11-01 17:57:08 +0800

    上传代码

--- Sorts/Bubble_Sort.py
+++ Sorts/Bubble_Sort.py
@@ -6,9 +6,10 @@
     两个数比较大小，较大的数下沉，较小的数冒起来。
 
 过程：
-    1. 比较相邻的两个数据，如果第二个数小，就交换位置。
-    2. 从后向前两两比较，一直到比较最前两个数据。最终最小数被交换到起始的位置，这样第一个最小数的位置就排好了。
+    1. 从前向后两两比较，一直到比较最后两个数据
+    2. 比较相邻的两个数据，如果前一个数大，就交换位置。每次都可以找出当前最大值.
     3. 继续重复上述过程，依次将第2.3...n-1个最小数排好位置。
+    4. 最终最小数被交换到起始的位置，这样第一个最小数的位置就排好了。
 
 平均时间复杂度：
     O(n2)
@@ -16,15 +17,15 @@
 
 def Bubble_Sort(datas):
     for i in range(1, len(datas)): # 用于记录遍历的次数, 由于自己不需要与自己比较,所以最多为len(datas)-1次
-        for j in range(len(datas)-i):
-            if datas[j] > datas[j+1]:
+        for j in range(len(datas)-i):   # 用于遍历剩余没遍历过的点
+            if datas[j] > datas[j+1]:   # 比较俩个数,如果前面数大,则交换俩个数
                 datas[j], datas[j+1] = datas[j+1], datas[j]
-
     return datas
+
 
 if __name__ == '__main__':
     import random
-    a = list(range(20))
+    a = list(range(10))
     random.shuffle(a)
     print(a)
     a = Bubble_Sort(a)
--- Sorts/Selction_Sort.py
+++ Sorts/Selction_Sort.py
@@ -3,7 +3,7 @@
 
 """
 基本思想：
-    在长度为N的无序数组中，第一次遍历n-1个数，找到最大的数值与第一个元素交换；
+    在长度为N的无序数组中，第一次遍历n-1个数，找到最小的数值与第一个元素交换；
     第二次遍历n-2个数，找到最小的数值与第二个元素交换；
     ...
     第n-1次遍历，找到最小的数值与第n-1个元素交换，排序完成。
@@ -17,10 +17,23 @@
 
 def Selction_Sort(datas):
     for i in range(len(datas)-1):
-        max = i # 第一次从第一个数开始,第二次从第二个数开始
+        min = i # 第一次从第一个数开始,第二次从第二个数开始
         for j in range(i+1, len(datas)):    # 用记录的max数与其他数挨着作比较
-            if datas[j] > datas[i]:
-                max = j
-        datas[i], datas[max] = datas[max], datas[i]
+            if datas[j] < datas[min]:
+                min = j
 
+            if min!=i:
+                datas[i], datas[min] = datas[min], datas[i]
+
+            print(datas)
+        print('---'*10)
     return datas
+
+
+if __name__ == '__main__':
+    import random
+    a = list(range(10))
+    random.shuffle(a)
+    print(a)
+    a = Selction_Sort(a)
+    print(a)
